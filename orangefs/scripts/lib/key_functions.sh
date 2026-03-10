da16ce7e067372e44ebadd55ee797484e73d6eb3
Hariharan <mani.hariharan@gmail.com>
2026-03-09 20:25:03 -0700

    Refactor SSH key setup to use a dedicated function for improved code organization and maintainability

--- orangefs/scripts/lib/key_functions.sh
+++ orangefs/scripts/lib/key_functions.sh
@@ -27,20 +27,28 @@ setup_ssh_keys() {
   local public_key="$1"
   local private_key="$2"
 
-  mkdir -p /home/cc/.ssh
-  echo "$public_key" > /home/cc/.ssh/id_rsa.pub
-  echo "-----BEGIN OPENSSH PRIVATE KEY-----" > /home/cc/.ssh/id_rsa
-  echo "$private_key" | tr -d " " | fold -w 70 >> /home/cc/.ssh/id_rsa
-  echo "-----END OPENSSH PRIVATE KEY-----" >> /home/cc/.ssh/id_rsa
-  cat /home/cc/.ssh/id_rsa.pub >> /home/cc/.ssh/authorized_keys
+  setup_user_ssh_keys() {
+    local ssh_dir="$1"
+    local owner="$2"
 
-  cat <<EOF >> /home/cc/.ssh/config
+    mkdir -p "$ssh_dir"
+    echo "$public_key" > "$ssh_dir/id_rsa.pub"
+    echo "-----BEGIN OPENSSH PRIVATE KEY-----" > "$ssh_dir/id_rsa"
+    echo "$private_key" | tr -d " " | fold -w 70 >> "$ssh_dir/id_rsa"
+    echo "-----END OPENSSH PRIVATE KEY-----" >> "$ssh_dir/id_rsa"
+    cat "$ssh_dir/id_rsa.pub" >> "$ssh_dir/authorized_keys"
+
+    cat <<EOF > "$ssh_dir/config"
 Host 10.52.*
     StrictHostKeyChecking no
     UserKnownHostsFile /dev/null
 EOF
 
-  chown -R cc:cc /home/cc/.ssh
-  chmod 700 /home/cc/.ssh
-  chmod 600 /home/cc/.ssh/id_rsa /home/cc/.ssh/id_rsa.pub /home/cc/.ssh/authorized_keys /home/cc/.ssh/config
+    chown -R "$owner" "$ssh_dir"
+    chmod 700 "$ssh_dir"
+    chmod 600 "$ssh_dir/id_rsa" "$ssh_dir/id_rsa.pub" "$ssh_dir/authorized_keys" "$ssh_dir/config"
+  }
+
+  setup_user_ssh_keys "/home/cc/.ssh" "cc:cc"
+  setup_user_ssh_keys "/root/.ssh" "root:root"
 }
